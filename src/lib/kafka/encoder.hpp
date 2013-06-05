6420db9ca6b8f7c3de75346ea5cabc7b13df9dce
Ben Gray <ben.gray@mediasift.com>
2013-06-05 10:36:37 +0100

    tweak to compile options and enum converstion to support g++ 4.4

--- src/lib/kafka/encoder.hpp
+++ src/lib/kafka/encoder.hpp
@@ -137,8 +137,9 @@ void request(std::ostream& stream, std::string const& topic, uint32_t const part
 	// Packet format is ... request size (4 bytes)
 	encoder::raw(stream, htonl(request_size));
 
-	// ... magic number (2 bytes)
-	encoder::raw(stream, htons(request_type::produce));
+	// ... request_type (2 bytes)
+	uint16_t type = static_cast<uint16_t>(request_type::produce); // some compilers don't like enum class yet
+	encoder::raw(stream, htons(type));
 
 	// ... topic string size (2 bytes) & topic string
 	encoder::raw(stream, htons(topic.size()));
